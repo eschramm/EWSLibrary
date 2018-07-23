5f1b9b6160a4e3a268cfccbd01cdaae99353c561
Eric Schramm <schramm@careevolution.com>
2018-07-23 14:01:39 -0500

    Initial Commit

---
+++ EWSLibrary_iOS/EWSLibrary_iOS.h
@@ -0,0 +1,19 @@
+//
+//  EWSLibrary_iOS.h
+//  EWSLibrary_iOS
+//
+//  Created by Eric Schramm on 7/23/18.
+//  Copyright © 2018 eware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EWSLibrary_iOS.
+FOUNDATION_EXPORT double EWSLibrary_iOSVersionNumber;
+
+//! Project version string for EWSLibrary_iOS.
+FOUNDATION_EXPORT const unsigned char EWSLibrary_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EWSLibrary_iOS/PublicHeader.h>
+
+
