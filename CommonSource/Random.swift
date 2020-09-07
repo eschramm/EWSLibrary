c8cfb611df625c87b1fe05d4183a6a8e4472e647
Eric Schramm <eschramm@mac.com>
2020-09-07 12:01:02 -0500

    add EWSStats

--- CommonSource/Random.swift
+++ CommonSource/Random.swift
@@ -38,3 +38,106 @@ public func randomBool(withTrueProbability trueProbability: Double) -> Bool {
     let random = Int.random(in: 0..<precision)
     return Double(random) < Double(precision) * trueProbability
 }
+
+public struct EWSStats<T:FloatingPoint> {
+    let sum: T
+    let min: T?
+    let max: T?
+    let mean: T?
+    let median: T?
+    let standardDeviation: T?
+    let relativeDeviation: T?
+    
+    enum Stat {
+        case sum
+        case min
+        case max
+        case mean
+        case median
+        case stDev
+        case relDev
+    }
+    
+    func string(stat: Stat, numberFormatter: NumberFormatter, naString: String = "--") -> String {
+        switch stat {
+        case .sum:
+            return numberFormatter.string(for: sum)!
+        case .min:
+            return numberFormatter.string(for: min) ?? naString
+        case .max:
+            return numberFormatter.string(for: max) ?? naString
+        case .mean:
+            return numberFormatter.string(for: mean) ?? naString
+        case .median:
+            return numberFormatter.string(for: median) ?? naString
+        case .stDev:
+            return numberFormatter.string(for: standardDeviation) ?? naString
+        case .relDev:
+            if let relDev = relativeDeviation {
+                return "\(numberFormatter.string(for: relDev * 100)!) %"
+            } else {
+                return naString
+            }
+        }
+    }
+}
+
+// MARK - Statistical Simple Calculations
+
+public extension Collection where Element: FloatingPoint {
+    
+    func stats() -> EWSStats<Element> {
+        let sortedSelf = sorted()
+        let sum = self.sum()
+        let mean = self.mean(sum: sum)
+        let median = self.median(sorted: sortedSelf)
+        let min = self.min(sorted: sortedSelf)
+        let max = self.max(sorted: sortedSelf)
+        let stDev = self.standardDeviation(mean: mean)
+        let relDev = self.relativeDeviation(stDev: stDev, mean: mean)
+        return EWSStats(sum: sum, min: min, max: max, mean: mean, median: median, standardDeviation: stDev, relativeDeviation: relDev)
+    }
+    
+    func sum() -> Element {
+        return self.reduce(0, +)
+    }
+    
+    func mean(sum: Element? = nil) -> Element? {
+        guard count > 0 else { return nil }
+        let sum = sum ?? self.sum()
+        return sum / Element(count)
+    }
+    
+    func min(sorted: [Element]? = nil) -> Element? {
+        let sorted = sorted ?? self.sorted()
+        return sorted.first
+    }
+    
+    func max(sorted: [Element]? = nil) -> Element? {
+        let sorted = sorted ?? self.sorted()
+        return sorted.last
+    }
+    
+    func median(sorted: [Element]? = nil) -> Element? {
+        guard count > 0 else { return nil }
+        let sorted = sorted ?? self.sorted()
+        if sorted.count % 2 == 0 {
+            return (sorted[sorted.count / 2] + sorted[sorted.count / 2 - 1]) / 2
+        } else {
+            return sorted[(sorted.count - 1) / 2]
+        }
+    }
+    
+    func standardDeviation(mean: Element? = nil) -> Element? {
+        guard count > 1 else { return nil }
+        let mean = mean ?? self.mean()!
+        let variance = self.reduce(0, { $0 + ($1 - mean) * ($1 - mean) })
+        return sqrt(variance / (Element(count) - 1))
+    }
+    
+    func relativeDeviation(stDev: Element? = nil, mean: Element? = nil) -> Element? {
+        guard let stDev = stDev ?? standardDeviation() else { return nil }
+        let mean = mean ?? self.mean()!
+        return stDev / mean
+    }
+}
