2804650b4882b81f17da9460c0712a8479a4eb34
Eric Schramm <eschramm@mac.com>
2023-12-26 21:26:59 -0600

    Date extension enhancements
    - add binary search to find next index for [Date]
    - add contained inside intervals for [DateInterval]

--- Sources/EWSLibrary/Date.swift
+++ Sources/EWSLibrary/Date.swift
@@ -8,12 +8,118 @@
 import Foundation
 
 public extension Date {
+    
     func truncateToNoon(calendar: Calendar = .current) -> Date {
         return calendar.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
     }
     
     func truncateToMidnight(calendar: Calendar = .current) -> Date? {
         return calendar.date(bySettingHour: 0, minute: 0, second: 0, of: self)
+    }
+    
+    /// NOTE: returns sortedDates.count (index that doesn't exist) for case where date exceeds the last in the array
+    func nextIndex(for sortedDates: [Date]) -> (nextIndex: Int, iterations: Int) {
+        // binary search
+        var iterations = 0
+        var left = 0
+        var right = sortedDates.count - 1
+        var middle = -1
+        var foundIndex = -1
+        
+        while left <= right {
+            iterations += 1
+            middle = (left + right) / 2
+            if sortedDates[middle] < self {
+                left = middle + 1
+            } else if sortedDates[middle] > self {
+                right = middle - 1
+            } else {
+                foundIndex = middle
+                break
+            }
+        }
+        
+        if foundIndex == -1 {  // not found, right should be just last index before nextDate
+            foundIndex = right
+        }
+        return (foundIndex + 1, iterations)
+    }
+    
+    struct InsideIntervalsRunInfo {
+        public let iterations: Int
+        public let closestIntervals: [DateInterval]
+    }
+    
+    func containedInsideIntervals(sortedIntervals: [DateInterval]) -> (isContainedInAnInterval: Bool, runInfo: InsideIntervalsRunInfo) {
+        
+        var iterations = 0
+        
+        guard !sortedIntervals.isEmpty else {
+            return (false, .init(iterations: 0, closestIntervals: []))
+        }
+        guard self <= sortedIntervals.last!.end else {
+            return (false, .init(iterations: 0, closestIntervals: []))
+        }
+        guard self >= sortedIntervals[0].start else {
+            return (false, .init(iterations: 0, closestIntervals: []))
+        }
+        
+        // binary search
+        var left = 0
+        var right = sortedIntervals.count - 1
+        var middle = -1
+        
+        while left <= right {
+            middle = (left + right) / 2
+            // print("\(left) - \(middle) - \(right)")
+            let interval = sortedIntervals[middle]
+            if interval.contains(self) {
+                return (true, .init(iterations: iterations, closestIntervals: [interval]))
+            } else if interval.end < self {
+                left = middle + 1
+            } else if interval.start > self {
+                right = middle - 1
+            } else {
+                break
+            }
+            iterations += 1
+        }
+        
+        return (false, .init(iterations: iterations, closestIntervals: [sortedIntervals[min(left, right)], sortedIntervals[max(left, right)]]))
+    }
+}
+
+public extension Array where Element == DateInterval {
+    
+    func hasOverlaps() -> Bool {
+        return !zip(self, self.dropFirst()).allSatisfy({ $0.0.end <= $0.1.start })
+    }
+    
+    func overlappingIntervals(printToConsole: Bool) -> [(Int, Int)] {
+        let hasOverlaps = hasOverlaps()
+        if hasOverlaps {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS ZZZ"
+            if printToConsole {
+                print("DateInterval array contains overlaps")
+            }
+            var overlaps = [(Int, Int)]()
+            for idx in 1..<count {
+                let first = self[idx - 1]
+                let second = self[idx]
+                if first.end > second.start {
+                    if printToConsole {
+                        print("[\(idx - 1)] : \(dateFormatter.string(from: first.start)) - \(dateFormatter.string(from: first.end))")
+                        print("[\(idx)] : \(dateFormatter.string(from: second.start)) - \(dateFormatter.string(from: second.end))")
+                        print("-------")
+                    }
+                    overlaps.append((idx - 1, idx))
+                }
+            }
+            return overlaps
+        } else {
+            return []
+        }
     }
 }
 
---
+++ Tests/EWSLibraryTests/DateTests.swift
@@ -0,0 +1,284 @@
+//
+//  DateTests.swift
+//  
+//
+//  Created by Eric Schramm on 11/29/23.
+//
+
+import XCTest
+
+extension Date {
+    func bruteForceContainedInsideIntervals(intervals: [DateInterval]) -> Bool {
+        var iterations = 0
+        
+        /*
+        defer {
+            print("Iterations 🐢: \(iterations)")
+        }
+         */
+        
+        for interval in intervals {
+            iterations += 1
+            if interval.contains(self) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func bruteForceNextIndex(for sortedDates: [Date]) -> Int {
+        var iterations = 0
+        for date in sortedDates {
+            iterations += 1
+            if self < date {
+                return iterations - 1
+            }
+        }
+        return iterations
+    }
+}
+
+final class DateBinarySearchTests: XCTestCase {
+    var df = DateFormatter()
+    var dates = [Date]()
+    
+    override func setUpWithError() throws {
+        
+        df.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        dates = [
+            df.date(from: "2023-01-01 00:00:00")!,
+            df.date(from: "2023-01-03 00:00:00")!,
+            df.date(from: "2023-01-05 00:00:00")!,
+            df.date(from: "2023-01-07 00:00:00")!,
+            df.date(from: "2023-01-09 00:00:00")!,
+            df.date(from: "2023-01-11 00:00:00")!,
+            df.date(from: "2023-01-13 00:00:00")!,
+            df.date(from: "2023-01-15 00:00:00")!,
+            df.date(from: "2023-01-17 00:00:00")!,
+            df.date(from: "2023-01-19 00:00:00")!,
+            df.date(from: "2023-01-21 00:00:00")!,
+            df.date(from: "2023-01-23 00:00:00")!,
+            df.date(from: "2023-01-25 00:00:00")!,
+            df.date(from: "2023-01-27 00:00:00")!,
+            df.date(from: "2023-01-29 00:00:00")!,
+            df.date(from: "2023-01-31 00:00:00")!,
+            df.date(from: "2023-03-01 00:00:00")!,
+            df.date(from: "2023-03-03 00:00:00")!,
+            df.date(from: "2023-03-05 00:00:00")!,
+            df.date(from: "2023-03-07 00:00:00")!,
+        ]
+    }
+    
+    func testBinarySearchChecker() throws {
+        try testBinaryDateSearch(for: df.date(from: "2023-01-01 12:00:00")!, expectation: 1)
+        try testBinaryDateSearch(for: df.date(from: "2023-01-29 12:00:00")!, expectation: 15)
+        try testBinaryDateSearch(for: df.date(from: "2023-01-02 12:00:00")!, expectation: 1)
+        try testBinaryDateSearch(for: df.date(from: "2022-01-02 12:00:00")!, expectation: 0)
+        try testBinaryDateSearch(for: df.date(from: "2024-01-02 12:00:00")!, expectation: 20)
+        try testBinaryDateSearch(for: df.date(from: "2023-01-01 00:00:00")!, expectation: 1)  // equal to a date in array, still return the next item
+    }
+    
+    func testBinaryDateSearch(for date: Date, expectation: Int) throws {
+        XCTAssertEqual(date.nextIndex(for: dates).nextIndex, expectation)
+        XCTAssertEqual(date.bruteForceNextIndex(for: dates), expectation)
+    }
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        let datesToEvaluate: [Date] = [
+            df.date(from: "2023-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 12:00:00")!,
+            df.date(from: "2023-01-03 12:00:00")!,
+            df.date(from: "2023-01-04 12:00:00")!,
+            df.date(from: "2023-01-05 12:00:00")!,
+            df.date(from: "2023-01-06 12:00:00")!,
+            df.date(from: "2023-02-01 12:00:00")!,
+            df.date(from: "2023-03-01 12:00:00")!,
+            df.date(from: "2023-03-02 12:00:00")!,
+            df.date(from: "2023-03-03 12:00:00")!,
+            df.date(from: "2023-03-04 12:00:00")!,
+            df.date(from: "2023-03-05 12:00:00")!,
+            df.date(from: "2023-03-06 12:00:00")!,
+            df.date(from: "2022-01-01 12:00:00")!,
+            df.date(from: "2024-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 00:00:00")!,
+            df.date(from: "2023-01-03 00:00:00")!,
+            df.date(from: "2023-01-28 12:00:00")!,
+            df.date(from: "2023-01-29 12:00:00")!,
+        ]
+        measure {
+            for _ in 0..<1000 {
+                for date in datesToEvaluate {
+                    _ = date.nextIndex(for: dates)
+                }
+            }
+        }
+    }
+    
+    func testBruteForcePerformanceExample() throws {
+        // This is an example of a performance test case.
+        let datesToEvaluate: [Date] = [
+            df.date(from: "2023-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 12:00:00")!,
+            df.date(from: "2023-01-03 12:00:00")!,
+            df.date(from: "2023-01-04 12:00:00")!,
+            df.date(from: "2023-01-05 12:00:00")!,
+            df.date(from: "2023-01-06 12:00:00")!,
+            df.date(from: "2023-02-01 12:00:00")!,
+            df.date(from: "2023-03-01 12:00:00")!,
+            df.date(from: "2023-03-02 12:00:00")!,
+            df.date(from: "2023-03-03 12:00:00")!,
+            df.date(from: "2023-03-04 12:00:00")!,
+            df.date(from: "2023-03-05 12:00:00")!,
+            df.date(from: "2023-03-06 12:00:00")!,
+            df.date(from: "2022-01-01 12:00:00")!,
+            df.date(from: "2024-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 00:00:00")!,
+            df.date(from: "2023-01-03 00:00:00")!,
+            df.date(from: "2023-01-28 12:00:00")!,
+            df.date(from: "2023-01-29 12:00:00")!,
+        ]
+        measure {
+            for _ in 0..<1000 {
+                for date in datesToEvaluate {
+                    _ = date.bruteForceNextIndex(for: dates)
+                }
+            }
+        }
+    }
+}
+
+final class DateIntervalSearchTests: XCTestCase {
+    
+    var df = DateFormatter()
+    var intervals = [DateInterval]()
+
+    override func setUpWithError() throws {
+        
+        df.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        intervals = [
+            .init(start: df.date(from: "2023-01-01 00:00:00")!, end: df.date(from: "2023-01-02 00:00:00")!),
+            .init(start: df.date(from: "2023-01-03 00:00:00")!, end: df.date(from: "2023-01-04 00:00:00")!),
+            .init(start: df.date(from: "2023-01-05 00:00:00")!, end: df.date(from: "2023-01-06 00:00:00")!),
+            .init(start: df.date(from: "2023-01-07 00:00:00")!, end: df.date(from: "2023-01-08 00:00:00")!),
+            .init(start: df.date(from: "2023-01-09 00:00:00")!, end: df.date(from: "2023-01-10 00:00:00")!),
+            .init(start: df.date(from: "2023-01-11 00:00:00")!, end: df.date(from: "2023-01-12 00:00:00")!),
+            .init(start: df.date(from: "2023-01-13 00:00:00")!, end: df.date(from: "2023-01-14 00:00:00")!),
+            .init(start: df.date(from: "2023-01-15 00:00:00")!, end: df.date(from: "2023-01-16 00:00:00")!),
+            .init(start: df.date(from: "2023-01-17 00:00:00")!, end: df.date(from: "2023-01-18 00:00:00")!),
+            .init(start: df.date(from: "2023-01-19 00:00:00")!, end: df.date(from: "2023-01-20 00:00:00")!),
+            .init(start: df.date(from: "2023-01-21 00:00:00")!, end: df.date(from: "2023-01-22 00:00:00")!),
+            .init(start: df.date(from: "2023-01-23 00:00:00")!, end: df.date(from: "2023-01-24 00:00:00")!),
+            .init(start: df.date(from: "2023-01-25 00:00:00")!, end: df.date(from: "2023-01-26 00:00:00")!),
+            .init(start: df.date(from: "2023-01-27 00:00:00")!, end: df.date(from: "2023-01-28 00:00:00")!),
+            .init(start: df.date(from: "2023-01-29 00:00:00")!, end: df.date(from: "2023-01-30 00:00:00")!),
+            .init(start: df.date(from: "2023-01-31 00:00:00")!, end: df.date(from: "2023-02-01 00:00:00")!),
+            .init(start: df.date(from: "2023-03-01 00:00:00")!, end: df.date(from: "2023-03-02 00:00:00")!),
+            .init(start: df.date(from: "2023-03-03 00:00:00")!, end: df.date(from: "2023-03-04 00:00:00")!),
+            .init(start: df.date(from: "2023-03-05 00:00:00")!, end: df.date(from: "2023-03-06 00:00:00")!),
+            .init(start: df.date(from: "2023-03-07 00:00:00")!, end: df.date(from: "2023-03-08 00:00:00")!),
+        ]
+    }
+
+    func testIntervalChecker() throws {
+        try testInterval(for: df.date(from: "2023-01-01 12:00:00")!, expectation: true)
+        try testInterval(for: df.date(from: "2023-01-29 12:00:00")!, expectation: true)
+        try testInterval(for: df.date(from: "2023-01-02 12:00:00")!, expectation: false)
+        try testInterval(for: df.date(from: "2022-01-02 12:00:00")!, expectation: false)
+        try testInterval(for: df.date(from: "2024-01-02 12:00:00")!, expectation: false)
+        try testInterval(for: df.date(from: "2023-01-01 00:00:00")!, expectation: true)  // equal to edge of end of interval - start
+        try testInterval(for: df.date(from: "2023-01-02 00:00:00")!, expectation: true)  // equal to edge of end of interval - end
+    }
+    
+    func testInterval(for date: Date, expectation: Bool) throws {
+        XCTAssertEqual(date.containedInsideIntervals(sortedIntervals: intervals).0, expectation)
+        XCTAssertEqual(date.bruteForceContainedInsideIntervals(intervals: intervals), expectation)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        let datesToEvaluate: [Date] = [
+            df.date(from: "2023-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 12:00:00")!,
+            df.date(from: "2023-01-03 12:00:00")!,
+            df.date(from: "2023-01-04 12:00:00")!,
+            df.date(from: "2023-01-05 12:00:00")!,
+            df.date(from: "2023-01-06 12:00:00")!,
+            df.date(from: "2023-02-01 12:00:00")!,
+            df.date(from: "2023-03-01 12:00:00")!,
+            df.date(from: "2023-03-02 12:00:00")!,
+            df.date(from: "2023-03-03 12:00:00")!,
+            df.date(from: "2023-03-04 12:00:00")!,
+            df.date(from: "2023-03-05 12:00:00")!,
+            df.date(from: "2023-03-06 12:00:00")!,
+            df.date(from: "2022-01-01 12:00:00")!,
+            df.date(from: "2024-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 00:00:00")!,
+            df.date(from: "2023-01-03 00:00:00")!,
+            df.date(from: "2023-01-28 12:00:00")!,
+            df.date(from: "2023-01-29 12:00:00")!,
+        ]
+        measure {
+            for _ in 0..<1000 {
+                for date in datesToEvaluate {
+                    _ = date.containedInsideIntervals(sortedIntervals: intervals)
+                }
+            }
+        }
+    }
+    
+    func testBruteForcePerformanceExample() throws {
+        // This is an example of a performance test case.
+        let datesToEvaluate: [Date] = [
+            df.date(from: "2023-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 12:00:00")!,
+            df.date(from: "2023-01-03 12:00:00")!,
+            df.date(from: "2023-01-04 12:00:00")!,
+            df.date(from: "2023-01-05 12:00:00")!,
+            df.date(from: "2023-01-06 12:00:00")!,
+            df.date(from: "2023-02-01 12:00:00")!,
+            df.date(from: "2023-03-01 12:00:00")!,
+            df.date(from: "2023-03-02 12:00:00")!,
+            df.date(from: "2023-03-03 12:00:00")!,
+            df.date(from: "2023-03-04 12:00:00")!,
+            df.date(from: "2023-03-05 12:00:00")!,
+            df.date(from: "2023-03-06 12:00:00")!,
+            df.date(from: "2022-01-01 12:00:00")!,
+            df.date(from: "2024-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 00:00:00")!,
+            df.date(from: "2023-01-03 00:00:00")!,
+            df.date(from: "2023-01-28 12:00:00")!,
+            df.date(from: "2023-01-29 12:00:00")!,
+            df.date(from: "2023-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 12:00:00")!,
+            df.date(from: "2023-01-03 12:00:00")!,
+            df.date(from: "2023-01-04 12:00:00")!,
+            df.date(from: "2023-01-05 12:00:00")!,
+            df.date(from: "2023-01-06 12:00:00")!,
+            df.date(from: "2023-02-01 12:00:00")!,
+            df.date(from: "2023-03-01 12:00:00")!,
+            df.date(from: "2023-03-02 12:00:00")!,
+            df.date(from: "2023-03-03 12:00:00")!,
+            df.date(from: "2023-03-04 12:00:00")!,
+            df.date(from: "2023-03-05 12:00:00")!,
+            df.date(from: "2023-03-06 12:00:00")!,
+            df.date(from: "2022-01-01 12:00:00")!,
+            df.date(from: "2024-01-01 12:00:00")!,
+            df.date(from: "2023-01-02 00:00:00")!,
+            df.date(from: "2023-01-03 00:00:00")!,
+            df.date(from: "2023-01-28 12:00:00")!,
+            df.date(from: "2023-01-29 12:00:00")!,
+        ]
+        measure {
+            for _ in 0..<1000 {
+                for date in datesToEvaluate {
+                    _ = date.bruteForceContainedInsideIntervals(intervals: intervals)
+                }
+            }
+        }
+    }
+
+}
+
